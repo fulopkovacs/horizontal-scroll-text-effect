99b2a0040b4141bbac9a6f56a18a4fa6af6005cb
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-10-22 13:21:08 +0200

    Add the star seperators

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,17 +1,47 @@
 import {motion, useScroll, useSpring, useTransform} from 'framer-motion'
-import {useMemo} from 'react'
+import React from 'react'
+
+function StarSeparator() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-32 aspect-square"
+      viewBox="0 0 242 242"
+    >
+      <g
+        className="stroke-current"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-width="10"
+      >
+        <path d="M121.5 0v242M0 120.5h242M36 35l171 171M36 207 207 36" />
+      </g>
+    </svg>
+  )
+}
 
 export function Title({children}: {children: string}) {
-  const text = new Array(20).fill(children).join(' ')
+  const repeatNTimes = 20
   const {scrollY} = useScroll()
   const translateX = useSpring(useTransform(() => scrollY.get() * -1 * 0.5))
 
   return (
     <motion.div
-      className="text-9xl text-neutral-50 whitespace-nowrap -ml-10"
+      className="text-9xl text-neutral-50 whitespace-nowrap -ml-10 flex items-center gap-3"
       style={{translateX}}
     >
-      {text}
+      {new Array(repeatNTimes).fill(1).map((_, i) => {
+        if (i === repeatNTimes - 1) {
+          return <div key={i}>{children}</div>
+        }
+
+        return (
+          <React.Fragment key={i}>
+            <div>{children}</div>
+            <StarSeparator />
+          </React.Fragment>
+        )
+      })}
     </motion.div>
   )
 }
