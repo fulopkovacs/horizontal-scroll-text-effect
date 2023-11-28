dad40febd75b4750eb6418d4a39419ea2c6e95e7
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-11-28 21:34:45 +0100

    [WIP] Update the animations

--- src/pages/v2/index.tsx
+++ src/pages/v2/index.tsx
@@ -1,5 +1,12 @@
-import { type ReactNode } from "react";
-import { motion } from "framer-motion";
+import { useRef, type ReactNode, useEffect, useState } from "react";
+import {
+  AnimatePresence,
+  delay,
+  motion,
+  useAnimate,
+  useAnimation,
+  useInView,
+} from "framer-motion";
 
 function Section({ children }: { children: ReactNode }) {
   return <div className="min-h-screen">{children}</div>;
@@ -30,20 +37,67 @@ function Card({
   repo: string;
   text: string;
 }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const inView = useInView(ref);
+  const controls = useAnimation();
+
+  const [entered, setEntered] = useState(false);
+
+  useEffect(() => {
+    if (inView && !entered) {
+      void controls.start("final");
+      setEntered(true);
+    }
+  }, [controls, inView]);
+
   return (
-    <motion.div
-      initial={{ width: 200, height: 200 }}
-      whileInView={{ width: 541, height: 432 }}
-      transition={{ duration: 0.3, delay: 0.2 }}
-      className="rounded-lg border border-gray12 px-20 py-7 shadow-lg shadow-gray1"
-    >
-      <h3 className="text-2xl font-semibold tracking-tight text-gray12">
-        {title}
-      </h3>
-      <p className="text-sm text-gray11">{repo}</p>
-      <p className="text-gray11">{text}</p>
-      <div className="h-[174px] w-[300px] rounded-lg" />
-    </motion.div>
+    <div ref={ref} className="w-full">
+      <AnimatePresence>
+        {entered ? (
+          <div className="flex w-full justify-center">
+            <motion.div
+              layout
+              layoutId={text}
+              initial={{ width: 200, height: 200, padding: "24px" }}
+              animate={{ width: 541, height: 432, padding: "80px" }}
+              transition={{ duration: 0.4, ease: "easeIn" }}
+              className="h-[432px] w-[541px] rounded-lg border border-gray12 shadow-lg shadow-gray1"
+            >
+              <motion.h3
+                layout
+                layoutId={title + "-title"}
+                className="text-2xl font-semibold tracking-tight text-gray12"
+              >
+                {title}
+              </motion.h3>
+              <p className="text-sm text-gray11">{repo}</p>
+              <p className="text-gray11">{text}</p>
+
+              <div className="h-[174px] w-[300px] rounded-lg" />
+            </motion.div>
+          </div>
+        ) : (
+          <div className="flex w-full justify-start">
+            <motion.div
+              layout
+              layoutId={title}
+              className="h-[200px] w-[200px] rounded-lg border border-gray12 p-6 shadow-lg shadow-gray1"
+            >
+              <motion.h3
+                layoutId={title + "-title"}
+                layout
+                className="text-2xl font-semibold tracking-tight text-gray12"
+              >
+                {title}
+              </motion.h3>
+              <p className="text-sm text-gray11">{repo}</p>
+
+              <div className="h-[174px] w-[300px] rounded-lg" />
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
